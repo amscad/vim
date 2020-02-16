9f910c27fd9a7c495d87230b27d4aaf521548795
amscad <ajmansfield@gmail.com>
2020-02-16 13:39:02 +0000

    modify setup script - needs to be ammended for the ipad

--- setup.sh
+++ setup.sh
@@ -2,19 +2,23 @@
 
 # Setup vim
 echo "Setting up vim"
-#remove old links (assuming they are links)
+
+# remove old links 
 rm -rf ~/.vim
 rm ~/.vimrc
+rm ~/.vimrc.bundles
 
-#setup new links to use
-mkdir -p ~/settings/vim
-ln -s ~/settings/vim ~/.vim
-ln -s ~/settings/vim/vimrc ~/.vimrc
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Running from $DIR"
+echo "Running on $OSTYPE"
+
+# setup new links to use
+ln -s "$DIR/vimrc" ~/.vimrc
+ln -s "$DIR/vimrc.mac.bundles" ~/.vimrc.bundles
 
 #install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/amscad/vim-plug/master/plug.vim
     
 vim +PlugInstall +qall 
-
-python3 ~/settings/vim/plugged/YouCompleteMe/install.py
